145937e95984973c530ec24947957ff6d11f9240
cclfly <1171407627@qq.com>
2017-06-10 21:30:47 +0800

    bilibili动态封面大图v0.1.2+圆角

--- bilibili动态封面大图.js
+++ bilibili动态封面大图.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili动态封面大图
 // @namespace    http://www.cclfly.cc/
-// @version      0.1
+// @version      0.1.2
 // @description  哔哩哔哩动态鼠标移入封面显示大图
 // @author       cclfly
 // @match        http://www.bilibili.com/
@@ -12,7 +12,7 @@
 $(function(){
     var oDiv = $('<div id="ccf-bilibili-surfaceImg"><img/></div>');
     oDiv.css({position:"fixed",right:"50vw",top:"50px",width:"40vw",height:"40vh","z-index":"10000",display:"none"});
-    oDiv.find("img").css({width:"100%"});
+    oDiv.find("img").css({width:"100%","border-radius":"2vmin"});
     oDiv.appendTo($("body"));
     var timer = null;
     $("body").on("mouseover","#dyn_wnd img",function(e){
