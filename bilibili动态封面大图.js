4afb3746eba165974101c88b92cb21c993461d2b
cclfly <1171407627@qq.com>
2017-06-11 00:36:40 +0800

    bilibili动态封面大图应用全站

--- bilibili动态封面大图.js
+++ bilibili动态封面大图.js
@@ -4,7 +4,7 @@
 // @version      0.1.2
 // @description  哔哩哔哩动态鼠标移入封面显示大图
 // @author       cclfly
-// @match        http://www.bilibili.com/
+// @match        http://www.bilibili.com/*
 // @grant        none
 // ==/UserScript==
 // @require http://code.jquery.com/jquery-3.2.1.min.js
