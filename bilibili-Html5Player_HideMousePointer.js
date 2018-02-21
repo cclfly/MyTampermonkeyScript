756188679a28ec292bb8f766cbe0436a91da8a70
Unknown <1171407627@qq.com>
2018-02-21 23:19:58 +0800

    更新鼠标隐藏算法

--- bilibili-Html5Player_HideMousePointer.js
+++ bilibili-Html5Player_HideMousePointer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         哔哩哔哩Html5播放器强制隐藏鼠标指针
 // @namespace    http://www.cclfly.cc/
-// @version      0.1.4
+// @version      0.1.5
 // @description  哔哩哔哩Html5播放器强制隐藏鼠标指针
 // @author       cclfly
 // @include        /^https?\:\/\/[\w]+.bilibili\.com/
@@ -10,19 +10,23 @@
 // @require http://code.jquery.com/jquery-3.2.1.min.js
 
 $(function() {
-    var mousePos = {x:0,y:0};
-    var timestamp = new Date().getTime();
-    $("body").on("mousemove",".bilibili-player-video-wrap",function(e){
-        xx = e.originalEvent.x || e.originalEvent.layerX || 0;
-        yy = e.originalEvent.y || e.originalEvent.layerY || 0;
-        if(mousePos.x==xx && mousePos.y==yy){
-            if(new Date().getTime()-timestamp>1200){
-                $(".bilibili-player-video").css("cursor",'url("data:image/ico;base64,AAABAAEAAQEAAAEAIAAwAAAAFgAAACgAAAABAAAAAgAAAAEAIAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAAAAA=="),default');
-            }
-        }else{
-            $(".bilibili-player-video").css("cursor","default");
-            timestamp = new Date().getTime();
+    var isMove  = false;
+    var timer   = null;
+    var fnTimer = function(){
+        console.log(isMove);
+        if(isMove == false && $(".bilibili-player-video").css("cursor") == 'default'){
+            $(".bilibili-player-video").css("cursor",'url("data:image/ico;base64,AAABAAEAAQEAAAEAIAAwAAAAFgAAACgAAAABAAAAAgAAAAEAIAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAAAAA=="),default');
         }
-        mousePos = {x:xx,y:yy};
+        isMove = false;
+    };
+    $("body").on("mouseover",".bilibili-player-video-wrap",function(e){
+        timer = setInterval(fnTimer, 1000);
+    });
+    $("body").on("mouseout",".bilibili-player-video-wrap",function(e){
+        clearInterval(timer);
+    });
+    $("body").on("mousemove",".bilibili-player-video-wrap",function(e){
+        isMove = true;
+        $(".bilibili-player-video").css("cursor",'default');
     });
 });
